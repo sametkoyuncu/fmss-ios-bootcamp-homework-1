58475ed72d714b9062e1de845c8afff7ce939fce
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-11 14:19:17 +0300

    Add delegation pattern for transfer data between two view controllers

--- homework1-bmi-calculator/ViewController.swift
+++ homework1-bmi-calculator/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     // outlets
     @IBOutlet weak var avatarView: UIView!
     @IBOutlet weak var avatarLabel: UILabel!
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         calculateButton.layer.cornerRadius = 30
         avatarView.layer.cornerRadius = 50
@@ -29,45 +31,48 @@ class ViewController: UIViewController {
     
     @IBAction func updateDataPressed(_ sender: UIButton) {
         welcomeView.isHidden = true
-        performSegue(withIdentifier: "goToModal", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToModal" {
-            let destinationVC = segue.destination as! ModalViewController
-            destinationVC.updateUserData = {[unowned self] data in
-                // veriables
-                let name = data.name
-                let height = data.height
-                let weight = data.weight
-                let theme = data.theme
-                
-                // calculate score
-                let score = weight / (height * height)
-                
-                // get score category
-                let scoreCategory = getCategoryData(for: score)
-                
-                // update ui
-                if let letter = name.first {
-                    self.avatarLabel.text = String(letter)
-                }
-                
-                self.avatarView.backgroundColor = theme
-                self.nameLabel.text = name
-                
-                self.heightValueLabel.text = String(format: "%.2f", height) + "m"
-                self.weightValueLabel.text = String(format: "%.1f", weight) + "kg"
-                
-                self.scoreValueLabel.text = String(format: "%.1f", score)
-                self.scoreCategoryLabel.text = scoreCategory.text
-                self.scoreCategoryLabel.textColor = scoreCategory.color
-                
-                self.calculateButton.backgroundColor = theme
+        let modalVC: ModalViewController = segue.destination as! ModalViewController
+        modalVC.delegate = self
+    }
+    
+}
+
+extension ViewController: ModalViewControllerDelegate {
+    func updateUser(data: UserModel) {
+        DispatchQueue.main.async { [weak self] in
+            let name = data.name
+            let height = data.height
+            let weight = data.weight
+            let theme = data.theme
+            
+            // calculate score
+            let score = weight / (height * height)
+            
+            // get score category
+            let scoreCategory = getCategoryData(for: score)
+            
+            // update ui
+            if let letter = name.first {
+                self?.avatarLabel.text = String(letter)
             }
+            
+            self?.avatarView.backgroundColor = theme
+            self?.nameLabel.text = name
+            
+            self?.heightValueLabel.text = String(format: "%.2f", height) + "m"
+            self?.weightValueLabel.text = String(format: "%.1f", weight) + "kg"
+            
+            self?.scoreValueLabel.text = String(format: "%.1f", score)
+            self?.scoreCategoryLabel.text = scoreCategory.text
+            self?.scoreCategoryLabel.textColor = scoreCategory.color
+            
+            self?.calculateButton.backgroundColor = theme
         }
     }
-
+    
 }
 
 struct Category {
